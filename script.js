152a6d8f014307a8e1b6f69d1f4921a1d06c1646
Seakibble <max.leeming@gmail.com>
2024-10-09 14:11:32 -0400

    Added Glossary

---
+++ script.js
@@ -0,0 +1,53 @@
+let $glossary = document.getElementById('glossary')
+let $search = document.getElementById('search')
+
+
+function getTerms() {
+    let search = $search.value
+
+    let output = ''
+    let terms = ''
+
+    let letter = null
+    let keys = Object.keys(judoTerms).sort()
+
+    if (search) {
+        keys = keys.filter((term) => {
+            return (term.includes(search) || judoTerms[term].includes(search))
+        })
+        output += `<p class='results'>${results(keys.length)}.</p>`
+    
+    } else {
+        output += `<p class='results'>All terms. ${results(keys.length)}.</p>`
+    }
+
+    keys.forEach(term => {
+        if (term[0] != letter) {
+            if (terms != '') {
+                output += `<div class='terms'>${terms}</div>`
+                terms = ''
+            }
+            letter = term[0]
+            output += '<h2>'+letter.toUpperCase()+'</h2>'
+        }
+
+        terms += `<p><strong>${term}</strong> - ${judoTerms[term]}</p>` 
+
+    })
+
+    if (terms != '') {
+        output += `<div class='terms'>${terms}</div>`
+        terms = ''
+    }
+
+    $glossary.innerHTML = output
+}
+getTerms()
+
+$search.addEventListener('input', getTerms)
+
+function results(n) {
+    if (n > 1) return n + ' results' 
+    else if (n == 1) return n + ' result'
+    else return 'No results'
+}
---
+++ terms.js
@@ -0,0 +1,130 @@
+const judoTerms = {
+    ashi: 'foot, leg',
+    'ashi waza': 'foot techniques',
+    arashi: 'storm',
+
+    dan: 'black belt grade',
+    de: 'advanced',
+    dojo: 'training room',
+
+    eri: 'lapel or collar',
+
+    gaeshi: 'counter',
+    gake: 'hooking, propping',
+    garami: 'lock',
+    gari: 'reaping',
+    gatame: 'hold',
+    goshi: 'hip',
+    guruma: 'wheel',
+    gyaku: 'reverse',
+    go: 'five',
+    gi: 'uniform',
+
+    ha: 'wing',
+    hajime: 'start',
+    hadaka: 'naked',
+    hane: 'springing',
+    harai: 'sweeping',
+    hiza: 'knee',
+    hachi: 'eight',
+    
+    ichi: 'one',
+    ippon: 'single-handed, point',
+
+    jigoku: 'hell',
+    jime: 'strangle',
+    juji: 'cross',
+    juu: 'ten',
+    judogi: 'Judo uniform',
+    judoka: 'Judo practitioner',
+    
+    kami: 'upper',
+    kata: 'single, form or pattern, shoulder, half',
+    kazure: 'modified',
+    kazushi: "Judo principle meaning 'off-balancing an opponent'",
+    keikoku: 'warning',
+    keiotsuke: 'attention',
+    kesa: 'scarf',
+    ko: 'small',
+    komi: 'pulling',
+    kyu: 'nine, coloured belt grade',
+
+    makikomi: 'wraparound',
+    mata: 'thigh',
+    maitta: 'I surrender',
+    mate: 'wait',
+    morote: 'two-handed',
+    
+    nage: 'throw',
+    nami: 'normal',
+    'ne waza': 'ground techniques',
+    ni: 'two',
+
+    o: 'big, full, great',
+    obi: 'belt',
+    okuri: 'sliding',
+    otoshi: 'drop',
+
+    randori: 'free sparring',
+    roku: 'six',
+    rei: 'bow',
+    
+    sankaku: 'entangled, triangle',
+    san: 'three',
+    sensei: 'teacher',
+    sesae: 'propping',
+    sode: 'sleeve',
+    soto: 'outer',
+    shiho: 'four quarter',
+    shiai: 'contest, tournament, sparring match',
+    shido: 'instruction, light penalty',
+    shodan: 'promotion, black belt',
+    sukui: 'scooping',
+    sutemi: 'sacrifice',
+    sumi: 'corner',
+    shi: 'four',
+    shichi: 'seven',
+    'seiryoku-zenyo': "Judo principle meaning 'good use of energy'",
+    
+    tatami: 'training mat',
+    te: 'hand',
+    tate: 'vertical',
+    'tachi waza': 'standing techniques',
+    tai: 'body',
+    tomoe: 'circle',
+    tsuri: 'lifting',
+    tori: 'attacking partner',
+
+    uchi: 'inner',
+    uchikomi: 'repetition training',
+    ude: 'arm',
+    uki: 'floating',
+    ukemi: 'breakfall',
+    uke: 'receiving partner',
+    ura: 'rear, back',
+    utsuri: 'shift',
+
+    waza: 'technique',
+
+    yama: 'mountain',
+    yoko: 'side',
+
+    'zempo-kaiten': 'forward',
+    zarei: 'kneeling bow'
+}
+/*
+Uchi - inner
+Uchikomi - repetition training 
+Ude - arm
+Uki - floating
+Ukemi - breakfall
+Uke - receiving partner
+Ura - rear
+
+Waza - technique
+
+Yama - Mountain
+Yoko - side
+
+Zempo-kaiten: forward breakfall 
+*/
